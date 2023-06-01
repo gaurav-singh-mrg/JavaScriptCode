75819d22169fb25c8be9cd2054a257344c759093
gaurav-singh-mrg <gauravsinghmr.g@gmail.com>
2023-06-01 07:51:18 +0530

    Fix2

--- app.js
+++ app.js
@@ -1,26 +1,8 @@
 //generate dynamic Html pages 
-
-
-// const body1 = document.getElementsByTagName("body");
 const div2 = document.createElement("div");
 const grids = div2.setAttribute("class", "grid");
-// div2.appendChild(grids);
 document.body.appendChild(div2);
-// const div1 = document.createElement("p");
-// div1.innerHTML = "Hello world ";
-// div1.addEventListener("click",  function(){
-//     if(this.innerHTML === "Hello Hello")
-//         this.innerHTML= "Hello World";
-//     else
-//         this.innerHTML= "Hello Hello";
-//     alert('Hello');
 
-
-// }, true);
-
-// document.body.appendChild(div1);
-// console.log("123");
-// const number = 9;
 createBoard(9)  //used to create board 
 const Allsquare = document.querySelectorAll('.square');
 mole();
@@ -34,35 +16,29 @@ function createBoard(number)
         div3.setAttribute("class", "square");
         div3.setAttribute("id", i);
         document.querySelector('.grid').appendChild(div3);
+        div3.addEventListener('click', function(){onClick(div3)});
     }
 }
-function onClickMole(id)
+
+function onClick(elm)
 {
-    console.log(id)
-
-    id.removeAttribute("class");
-    id.setAttribute("class", "square");
-    id.removeEventListener("click", function(){})
-    mole();
-    // clear();
-
+    if(elm.className === 'square mole')
+    {
+        elm.removeAttribute("class");
+        elm.setAttribute("class", "square");
+        mole();
+    }
+    else
+    {
+        //todo :
+    }
 }
-// function clear(){
-//     const a = document.querySelectorAll('.square mole')
-//     // alert(a.length)
-//     for(b = 0 ;b<a.length-1; b++)
-//     {
-//         a[b].removeAttribute("class")
-//         a[b].removeEventListener("click",function(){})
-//     }
-// }
+
 function mole()
 {
-    let random = Math.floor(Math.random() * Allsquare.length);
-    console.log(random);
+    let random = Math.floor(Math.random() * 9);
     const elem = document.getElementById(random);
     elem.setAttribute("class","square mole");
-    elem.addEventListener('click', function(){onClickMole(elem)});
 }
 
 
